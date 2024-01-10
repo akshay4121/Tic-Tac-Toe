f7e53277c710f3d6f33ff5bde6e94b8ca8d6665f
Akshay <akshaymkgm98@gmail.com>
2024-01-09 23:00:19 -0500

    Added Game Over functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol, isActive}){
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEdit(){
         setIsEditing((editing) => !isEditing);
     }
 
-    let PlayerName = <span className="player-name">{name}</span>;
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if(isEditing){
-        PlayerName = <input type="text" required value={name}/>;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
     }
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
         <span className="player">
-        { PlayerName}
+        { editablePlayerName}
         <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+        <button onClick={handleEdit} >{isEditing ? 'Save' : 'Edit'}</button>
        </li>
     );
 }
